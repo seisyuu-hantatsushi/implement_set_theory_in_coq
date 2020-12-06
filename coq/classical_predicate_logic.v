14a0fcea520c9e7bae18e07c0647f2cc298b5eca
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2020-12-06 14:50:51 +0900

    Modifing to import predicate_logic.

--- coq/classical_predicate_logic.v
+++ coq/classical_predicate_logic.v
@@ -1,28 +1,14 @@
 From mathcomp Require Import ssreflect.
 
-Require Import predicate_logic.
+Require Import  predicate_logic .
 
-Module ClassicalPredicatelLogic.
-  Axiom DoubleNegativeElimination: forall A:Prop, ~~A -> A.
+Axiom DoubleNegativeElimination: forall A:Prop, ~~A -> A.
 
-  Theorem DoubleNegativeLawOfExcludeMiddle: forall P:Prop, ~~(P \/ ~P).
-  Proof.
-    move => P H.
-    apply H.
-    right.
-    move => H0.
-    apply H.
-    left.
-    apply H0.
-  Qed.
+Theorem LawOfExcludeMiddle: forall P:Prop, P \/ ~P.
+Proof.
+  move => P.
+  apply DoubleNegativeElimination.
+  apply DoubleNegativeLawOfExcludeMiddle.
+Qed.
 
-  Theorem LawOfExcludeMiddle: forall P:Prop, P \/ ~P.
-  Proof.
-    move => P.
-    apply DoubleNegativeElimination.
-    apply DoubleNegativeLawOfExcludeMiddle.
-  Qed.
-
-End ClassicalPropositionalLogic.
-
-Export propositional_logic.
+Require Export predicate_logic.
--- coq/predicate_logic.v
+++ coq/predicate_logic.v
@@ -1,31 +1,45 @@
 From mathcomp Require Import ssreflect.
 
-Module PredicateLogic.
+(* 自同律 *)
+Theorem LawOfIdentity: forall A:Prop, A <-> A.
+Proof.
+  rewrite /iff.
+  split.
+  apply.
+  apply.
+Qed.
 
-  Section PredicateLogic.
+Theorem NotFalse : ~False.
+Proof.
+  rewrite /not.
+  apply.
+Qed.
 
-    Theorem LawOfIdentity: forall A:Prop, A <-> A.
-    Proof.
-      rewrite /iff.
-      split.
-      apply.
-      apply.
-    Qed.
+Theorem LawOfContradiction: forall P:Prop, ~(P /\ ~P).
+Proof.
+  move => P.
+  case.
+  move => H HN.
+  apply HN.
+  apply H.
+Qed.
 
-    Theorem NotFalse : ~False.
-    Proof.
-      rewrite /not.
-      apply.
-    Qed.
+Theorem DoubleNegative: forall A:Prop, A -> ~~A.
+Proof.
+  rewrite /not.
+  move => A HA HNA.
+  apply HNA.
+  apply HA.
+Qed.
 
-    Theorem DoubleNegative: forall A:Prop, A -> ~~A.
-    Proof.
-      rewrite /not.
-      move => A HA HNA.
-      apply HNA.
-      apply HA.
-    Qed.
+Theorem DoubleNegativeLawOfExcludeMiddle: forall P:Prop, ~~(P \/ ~P).
+Proof.
+  move => P H.
+  apply H.
+  right.
+  move => H0.
+  apply H.
+  left.
+  apply H0.
+Qed.
 
-  End PredicateLogic.
-
-End PredicateLogic.
