c62f982f3b097793cc41794a494a49ffde90187e
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2020-12-07 09:16:04 +0900

    appending operation laws.

--- coq/predicate_logic.v
+++ coq/predicate_logic.v
@@ -15,6 +15,7 @@ Proof.
   apply.
 Qed.
 
+(* 矛盾律 *)
 Theorem LawOfContradiction: forall P:Prop, ~(P /\ ~P).
 Proof.
   move => P.
@@ -24,12 +25,195 @@ Proof.
   apply H.
 Qed.
 
+(* 二重否定 *)
 Theorem DoubleNegative: forall A:Prop, A -> ~~A.
 Proof.
   rewrite /not.
   move => A HA HNA.
   apply HNA.
   apply HA.
+Qed.
+
+(* 冪等律 *)
+Theorem LawOfIdempotenceAtAnd: forall P:Prop, P /\ P <-> P.
+Proof.
+  move => P.
+  rewrite /iff.
+  split.
+  case.
+  move => HP.
+  apply.
+  move => HP.
+  split.
+  apply HP.
+  apply HP.
+Qed.
+
+Theorem LawOfIdempotenceAtOr: forall P:Prop, P \/ P <-> P.
+Proof.
+  move => P.
+  rewrite /iff.
+  split.
+  case.
+  apply.
+  apply.
+  move => HP.
+  right.
+  apply HP.
+Qed.
+
+Theorem LawOfCommutativeAtAnd: forall A B:Prop, A /\ B <-> B /\ A.
+Proof.
+  move => A B.
+  rewrite /iff.
+  split; case; move => H0 H1; split.
+  apply H1.
+  apply H0.
+  apply H1.
+  apply H0.
+Qed.
+
+Theorem LawOfCommutativeAtOr: forall A B:Prop, A \/ B <-> B \/ A.
+Proof.
+  move => A B.
+  rewrite /iff.
+  split; case; move => H.
+  right.
+  apply H.
+  left.
+  apply H.
+  right.
+  apply H.
+  left.
+  apply H.
+Qed.
+
+Theorem LawOfAssociateAtAnd: forall A B C:Prop, (A /\ B) /\ C <-> A /\ (B /\ C).
+Proof.
+  move => A B C.
+  rewrite /iff.
+  split; case.
+  case.
+  move => HA HB HC.
+  split.
+  apply HA.
+  split.
+  apply HB.
+  apply HC.
+  move => HA.
+  case.
+  move => HB HC.
+  split.
+  split.
+  apply HA.
+  apply HB.
+  apply HC.
+Qed.
+
+Theorem LawOfAssociateAtOr: forall A B C:Prop, (A \/ B) \/ C <-> A \/ (B \/ C).
+Proof.
+  move => A B C.
+  rewrite /iff.
+  split; case.
+  case; move => H.
+  left.
+  apply H.
+  right.
+  left.
+  apply H.
+  move => H.
+  right.
+  right.
+  apply H.
+  move => H.
+  left.
+  left.
+  apply H.
+  case; move => H.
+  left.
+  right.
+  apply H.
+  right.
+  apply H.
+Qed.
+
+Theorem LawOfDistributiveByOr: forall A B C:Prop, (A /\ B) \/ C <-> (A \/ C) /\ (B \/ C).
+Proof.
+  move => A B C.
+  rewrite /iff.
+  split.
+  case.
+  case.
+  move => HA HB.
+  split; left.
+  apply HA.
+  apply HB.
+  move => HC.
+  split; right; apply HC.
+  case; move => H0 H1.
+  case H0; move => H2.
+  case H1; move => H3.
+  left.
+  split.
+  apply H2.
+  apply H3.
+  right.
+  apply H3.
+  right.
+  apply H2.
+Qed.
+
+Theorem LawOfDistributiveByAnd: forall A B C:Prop, (A \/ B) /\ C <-> (A /\ C) \/ (B /\ C).
+Proof.
+  move => A B C.
+  rewrite /iff.
+  split; case; case; move => H0 H1.
+  left.
+  split.
+  apply H0.
+  apply H1.
+  right.
+  split.
+  apply H0.
+  apply H1.
+  split.
+  left.
+  apply H0.
+  apply H1.
+  split.
+  right.
+  apply H0.
+  apply H1.
+Qed.
+
+Theorem LawOfAbsorptionToOr: forall A B:Prop, (A /\ B) \/ A <-> A.
+Proof.
+  move => A B.
+  rewrite /iff.
+  split.
+  case.
+  case.
+  move => HA HB.
+  apply HA.
+  apply.
+  move => HA.
+  right.
+  apply HA.
+Qed.
+
+Theorem LawOfAbsorptionToAnd: forall A B:Prop, (A \/ B) /\ A <-> A.
+Proof.
+  move => A B.
+  rewrite /iff.
+  split.
+  case.
+  move => H0.
+  apply.
+  move => H0.
+  split.
+  left.
+  apply H0.
+  apply H0.
 Qed.
 
 Theorem DoubleNegativeLawOfExcludeMiddle: forall P:Prop, ~~(P \/ ~P).
